1164e9a1b289633b6f6102b8777606bb50d47d24
ComputerCrash0 <ComputerCrash0@gmail.com>
2017-06-02 12:12:35 -0400

    This is a clone of the game breakout

---
+++ Assets/Scripts/TimeDestory.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeDestory : MonoBehaviour
+{
+
+    public float destoryTime = 1f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Destroy(gameObject, destoryTime);
+	}
+}
